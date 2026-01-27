c95ecf2fbfd6ea26935cc61793209595300c6494
devdiagon <fredericktimo@gmail.com>
2026-01-27 12:17:00 -0500

    Lint corrections

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (_req, res) => {
-  res.send('Integracion continua funcionando!')
+  res.send('Integracion continua funcionando!');
 });
 
 app.listen(PORT, () => {
