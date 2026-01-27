03ba92149609c439a33a20806eab08a1e7e37df6
devdiagon <fredericktimo@gmail.com>
2026-01-27 11:47:58 -0500

    Proyecto base con CI

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const PORT = 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Integracion continua funcionando!')
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor esta ejecutandose en el puerto ${PORT}`);
+});
---
+++ sum.test.js
@@ -0,0 +1,5 @@
+import sum from './sum.js';
+
+test('Suma de 3 + 4 debe ser 7', () => {
+  expect(sum(3, 4)).toBe(7);
+});
